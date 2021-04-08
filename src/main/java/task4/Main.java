b8cc78a5cf89dcb42f220ccfbbb2b75bc60980a3
acmesun <danfyou@gmail.com>
2021-04-08 15:19:01 +0300

    creating users by parameters from arguments main

--- src/main/java/task4/Main.java
+++ src/main/java/task4/Main.java
@@ -1,19 +1,32 @@
 package task4;
 
+import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
 
 import java.util.Locale;
 
+
 public class Main {
     public static void main(String[] args) {
-        
-        Faker faker = new Faker(new Locale("ru_RU"));
+        int numberOfUsers = Integer.parseInt(args[0]);
+        String region = args[1];
+        for (int i = 0; i < numberOfUsers; i++) {
+            Faker faker = new Faker(new Locale(region));
+            String fullName = faker.name().fullName();
+            Address address = faker.address();
+            String code = address.zipCode();
+            String country = address.country();
+            String streetAddress = address.streetAddress();
+            String houseNumber = address.buildingNumber();
+            String phoneNumber = faker.phoneNumber().phoneNumber();
+            System.out.println(fullName + "; " + code + " " + country + ", " + streetAddress + ", " + houseNumber + "; " + phoneNumber);
+        }
+    }
+}
+
+/*Faker faker = new Faker(new Locale("ru_RU"));
         String zipCode = faker.address().zipCode();
         String address = faker.address().streetAddress();
         String name = faker.name().fullName();
         String telNumber = faker.phoneNumber().phoneNumber();
-        System.out.println(name + "; " + zipCode + " " + address + "; " + telNumber);
-
-    }
-}
-
+        System.out.println(name + "; " + zipCode + " " + address + "; " + telNumber); */
