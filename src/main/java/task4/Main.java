fb2e1ec5d165978366a6e1c733934bc8d73c8664
acmesun <danfyou@gmail.com>
2021-04-08 16:17:31 +0300

    add method to create russian fake users

--- src/main/java/task4/Main.java
+++ src/main/java/task4/Main.java
@@ -1,32 +1,38 @@
 package task4;
 
-import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
 
 import java.util.Locale;
+import java.util.Random;
 
 
 public class Main {
+    private static int numberOfUsers;
+    private static String region;
+
     public static void main(String[] args) {
-        int numberOfUsers = Integer.parseInt(args[0]);
-        String region = args[1];
+        numberOfUsers = Integer.parseInt(args[0]);
+        region = args[1];
+        switch (region) {
+            case "ru_RU":
+                ruFaker();
+        }
+
+    }
+
+    private static void ruFaker() {
+        Faker faker = new Faker(new Locale(region));
+        String fullName;
         for (int i = 0; i < numberOfUsers; i++) {
-            Faker faker = new Faker(new Locale(region));
-            String fullName = faker.name().fullName();
-            Address address = faker.address();
-            String code = address.zipCode();
-            String country = address.country();
-            String streetAddress = address.streetAddress();
-            String houseNumber = address.buildingNumber();
+            int randomInt = new Random().nextInt(2);
+            if (randomInt == 0) {
+                fullName = faker.expression("#{Name.male_first_name} #{Name.male_middle_name} #{Name.male_last_name}");
+            } else
+                fullName = faker.expression("#{Name.female_first_name} #{Name.female_middle_name} #{Name.female_last_name}");
+            String zipCode = faker.address().zipCode();
+            String address = faker.expression("#{Address.default_country}, #{Address.city}, #{Address.street_address} #{Address.secondary_address}");
             String phoneNumber = faker.phoneNumber().phoneNumber();
-            System.out.println(fullName + "; " + code + " " + country + ", " + streetAddress + ", " + houseNumber + "; " + phoneNumber);
+            System.out.println(fullName + "; " + zipCode + " " + address + "; " + phoneNumber);
         }
     }
 }
-
-/*Faker faker = new Faker(new Locale("ru_RU"));
-        String zipCode = faker.address().zipCode();
-        String address = faker.address().streetAddress();
-        String name = faker.name().fullName();
-        String telNumber = faker.phoneNumber().phoneNumber();
-        System.out.println(name + "; " + zipCode + " " + address + "; " + telNumber); */
